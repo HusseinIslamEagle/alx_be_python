a4597629ae2a98d615aa516aa8fbd78cee1583e9
Hussein Ahmed <aha.hussein3@gmail.com>
2025-11-30 16:58:34 +0200

    programming_paradigm

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,44 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False   # private attribute
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Return True if the book is not checked out."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []   # private books list
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False
+
+    def list_available_books(self):
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
